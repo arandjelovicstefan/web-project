a21f2c2b3a75bca10c4ac093d8d83bb1623510dc
arandjelovicstefan <87954129+arandjelovicstefan@users.noreply.github.com>
2021-11-09 23:34:00 +0100

    update 09.11

--- src/service.js
+++ src/service.js
@@ -1,17 +1,17 @@
 import axios from 'axios';
 
 export const getAllSectionItems = () => {
-   return axios.get('http://localhost:3006/sections');
+   return axios.get('https://web-project-itbootcamp.herokuapp.com/sections');
 };
 
 export const getAllShopItems = () => {
-   return axios.get('http://localhost:3006/shopItems');
+   return axios.get('https://web-project-itbootcamp.herokuapp.com/shopItems');
 };
 
 export const getSectionShopItems = nr => {
-   return axios.get(`http://localhost:3006/shopItems/${nr}`);
+   return axios.get(`https://web-project-itbootcamp.herokuapp.com/shopItems/${nr}`);
 };
 
 export const addSectionShopItem = (nr, item) => {
-   return axios.patch(`http://localhost:3006/shopItems/${nr}`, item);
+   return axios.patch(`https://web-project-itbootcamp.herokuapp.com/shopItems/${nr}`, item);
 };
